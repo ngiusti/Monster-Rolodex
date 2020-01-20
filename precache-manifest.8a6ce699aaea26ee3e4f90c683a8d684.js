13c985440b1976747a49ff65f95408df679371da
Nicholas Giusti <nicholas@ip-10-1-10-6.ec2.internal>
2020-01-20 15:15:30 -0800

    Updates

---
+++ precache-manifest.8a6ce699aaea26ee3e4f90c683a8d684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefeaa320afd37f8a52fb35373b4febf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb13e85f5f0d2ece5f14",
+    "url": "/static/css/main.5f5700a6.chunk.css"
+  },
+  {
+    "revision": "7e894316149ac95bff0e",
+    "url": "/static/js/2.994afdca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.994afdca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb13e85f5f0d2ece5f14",
+    "url": "/static/js/main.3d34d382.chunk.js"
+  },
+  {
+    "revision": "249a37921e0703ab7925",
+    "url": "/static/js/runtime-main.04abcc89.js"
+  }
+]);
